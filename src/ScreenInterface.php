12488c16adb9b532622e9cffdac82e7b9cb557a6
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-21 11:23:51 +0200

    Updated description for `ScreenInterface`

--- src/ScreenInterface.php
+++ src/ScreenInterface.php
@@ -7,8 +7,12 @@ use Traversable;
 /**
  * Something that represents a settings screen.
  *
- * A settings screen is a conceptual group of settings sections.
- * It's primary purpose is to render certain sections together in a single screen.
+ * A settings screen is a conceptual group of settings sections. It's primary purpose is to render certain sections
+ * together in a single screen.
+ *
+ * Note that a "screen" is not synonymous to a "page". In most environments, such as WordPress, a page is some
+ * registered URL that renders content while a "screen" is what is shown on that page at any given time, such as via
+ * tabs on that page.
  *
  * @since [*next-version*]
  */
