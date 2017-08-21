2581b21e47c17a92c39aa3b632ecbd58cd4939af
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-21 11:25:34 +0200

    Updated description for `FieldInterface`

--- src/FieldInterface.php
+++ src/FieldInterface.php
@@ -8,8 +8,9 @@ use Dhii\Validation\ValidatorInterface;
 /**
  * Something that represents a settings field that can be rendered.
  *
- * A settings field describes a single UI element that controls the display and/or adjustment of some value.
- * Hence, it's primary purpose is rendering the UI element.
+ * A settings field describes a UI component that displays and/or allows adjustment of some value.
+ * Hence, it's primary purpose is rendering the UI component. Through awareness of a {@link Dhii\Type\TypeInterface
+ * type}, validation of values is also possible.
  *
  * @since [*next-version*]
  */
